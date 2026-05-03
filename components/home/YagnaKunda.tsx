06526df39fca93e98335415b0510b776b1be3c81
Surya Prasad CA <suryaprasad5@gmail.com>
2026-05-03 22:29:58 +0530

    image change1

--- components/home/YagnaKunda.tsx
+++ components/home/YagnaKunda.tsx
@@ -45,8 +45,8 @@ export default function YagnaKunda() {
         <Image
           src="/images/yagna-kunda.png"
           alt="Yagna Kunda with sacred fire"
-          width={540}
-          height={540}
+          width={1040}
+          height={1040}
           style={{ objectFit: 'contain' }}
           priority
         />
